f08be24a72163c91471ae19f6c9334967dfbd843
TuanPham0610 <phamanhtuan06102003@gmail.com>
2024-01-17 18:03:54 +0700

    math3.java

--- src/math.java
+++ src/math.java
@@ -5,19 +5,19 @@ import java.util.Scanner; //Khai báo thư viện cho phép nhập đầu vào
 public class math {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in); //tạo đối tượng của lớp
+        Scanner sc = new Scanner(System.in); //tạo đối tượng để nhập
 
         System.out.print("Number 1: "); //Nhập số thứ nhất
-        int number_a = input.nextInt(); 
+        double number_a = sc.nextDouble(); 
    
         System.out.print("Number 2:  "); //Nhập số thứ hai
-        int number_b = input.nextInt();
+        double number_b = sc.nextDouble();
 
-        int number_c = number_a + number_b; //Cộng
-        int number_d = number_a - number_b; //Trừ
-        int number_e = number_a * number_b; //Nhân
-        int number_f = number_a / number_b; //Chia
-        int number_g = number_a % number_b; //Chia lấy phần dư
+        double number_c = number_a + number_b; //Cộng
+        double number_d = number_a - number_b; //Trừ
+        double number_e = number_a * number_b; //Nhân
+        double number_f = number_a / number_b; //Chia
+        double number_g = number_a % number_b; //Chia lấy phần dư
 
         System.out.println("____________________"); //Đầu ra kết quả
         System.out.println("Sum: "+number_c);           //Tổng
@@ -25,7 +25,22 @@ public class math {
         System.out.println("Multiplication: "+ number_e);//Tích
         System.out.println("Product: "+ number_f);  //Thương
         System.out.println("Remainder: "+ number_g);    //Phần dư
+
+
+        System.out.println("____________________");
+        System.out.println("Compare two numbers");
+
+        if (number_a > number_b)
+        {System.out.println(number_a+" > "+number_b);}
+
+        if (number_a < number_b)
+        {System.out.println(number_a+" < "+number_b);}
+
+        if (number_a == number_b)
+        {System.out.println(number_a+" = "+number_b);}
+
+        
      
-        input.close();//Đóng nhập
+        sc.close();//Đóng nhập
     }
 }
---
+++ src/math3.java
@@ -0,0 +1,31 @@
+// Giải phương trình bậc nhất
+
+import java.util.Scanner; 
+
+public class math3{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Number 1 : "); //Nhập số
+        double number_a = sc.nextDouble();
+
+        
+        System.out.print("Number 2: "); //Nhập số
+        double number_b = sc.nextDouble();
+
+
+        if (number_a == 0){
+            System.out.print("Khong phai phuong trinh bac nhat");
+        }
+        else {
+            System.out.println("Equation : "+ number_a + "X"+ " + " + number_b +"= 0");
+            double number_x = -number_b / number_a;
+            System.out.println("Nghiem X : " + number_x);
+
+        }
+
+        sc.close();
+    }
+
+}
